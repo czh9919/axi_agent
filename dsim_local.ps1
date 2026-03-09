ae318a020209275d0fb56a69af422ffee90aa241
Zihao Chang <czh9919@gmail.com>
2026-03-09 19:17:31 +0000

    Update .gitignore and dsim scripts for improved simulation support

--- dsim_local.ps1
+++ dsim_local.ps1
@@ -2,7 +2,7 @@
 # PowerShell script to simulate design with Altair® DSim™ on local machine
 
 # Analyze and Elaborate design
-dsim -top top_tb -genimage image -uvm 1.2 -f /tb/tb.f -f rtl/rtl.f -timescale 1ns/1ps
+dsim -top top_tb -genimage image -uvm 1.2 -f /tb/tb.f -f rtl/rtl.f -timescale 1ns/1ps +acc+b
 
 # Simulate design
-dsim -image image -uvm 1.2 +UVM_NO_RELNOTES +UVM_TESTNAME=axi_lite_base_test
+dsim -image image -uvm 1.2 -waves waves.mxd +UVM_NO_RELNOTES +UVM_TESTNAME=axi_lite_base_test
